30393287260f1b5e737f9ebc622dff116347d48a
modcomlearning <modcomlearning@gmail.com>
2021-06-10 12:05:05 +0300

    Third Commit

---
+++ checknummber.py
@@ -0,0 +1,12 @@
+
+# check if a number is positive, negative or zero
+x = int(input('Enter Your Number:  '))
+
+if x > 0:
+    print('Its Positive')
+
+elif x == 0:
+    print('Its Zero')
+
+else:
+    print('Its Negative')
